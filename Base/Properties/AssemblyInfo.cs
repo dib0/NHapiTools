9c1cbeeb93f07f5d646995396ccdd854645f0e6d
Bas van den Berg <bas@dib0.nl>
2020-06-29 16:10:51 +0200

    Correction on version info+file versions

--- Base/Properties/AssemblyInfo.cs
+++ Base/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NHapiTools.Base")]
-[assembly: AssemblyCopyright("Copyright Division by Zero ©  2013 - 2018")]
+[assembly: AssemblyCopyright("Copyright Division by Zero © 2013 - 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.11.0.0")]
-[assembly: AssemblyFileVersion("1.10.0.0")]
+[assembly: AssemblyVersion("1.11.0.2")]
+[assembly: AssemblyFileVersion("1.11.0.2")]
